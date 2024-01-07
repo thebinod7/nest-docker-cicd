19d93d9bd2ccf5d430ce90c86d991233b8dc1916
Binod Chaudhary <thebinod7@gmail.com>
2024-01-07 18:19:24 +0545

    update port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
-  const PORT = config.get('PORT');
+  const PORT = 5000;
   await app.listen(PORT);
   console.log(`Server running at http://localhost:${PORT}`);
 }
